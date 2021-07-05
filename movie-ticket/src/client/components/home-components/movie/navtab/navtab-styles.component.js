e0bffcb34270c2e18504cf16c40dfba02cd12196
hainguyen <minhhai123yn@gmail.com>
2021-07-06 01:02:50 +0700

    hainguyen fix tabs

--- movie-ticket/src/client/components/home-components/movie/navtab/navtab-styles.component.js
+++ movie-ticket/src/client/components/home-components/movie/navtab/navtab-styles.component.js
@@ -1,37 +1,44 @@
 import { makeStyles } from "@material-ui/core";
 
 export const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        margin: '30px 0px',
+  root: {
+    width: "100%",
+    margin: "30px 0px",
+  },
+  container: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+  appbar: {
+    backgroundColor: "white",
+    width: "320px",
+      [theme.breakpoints.down("xs")]: {
+        width: "296px",
+      },
+    color: "#797979",
+    "& .MuiAppBar-root": {
+      width: "320px",
+      [theme.breakpoints.down("xs")]: {
+        width: "296px",
+      },
     },
-    container: {
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
+    borderRadius: "5px",
+  },
+  tabs: {
+    "& .MuiTab-textColorInherit.Mui-selected": {
+      background:
+        "linear-gradient(to right, #4c46e6, #6d68fe, #b446ff, rgb(151, 0, 255))",
+      color: "white",
+      borderBottom: "none",
     },
-    appbar: {
-        backgroundColor: 'white',
-        color: '#797979',
-        width: '320px',
-        [theme.breakpoints.down('xs')]: {
-            width: '296px',
-        },
-        borderRadius: '5px',
+    "& .MuiTab-textColorInherit": {
+      "&:hover": {
+        background:
+          "linear-gradient(to right, #4c46e6, #6d68fe, #b446ff, rgb(151, 0, 255))",
+        color: "white",
+      },
     },
-    tabs: {
-        "& .MuiTab-textColorInherit.Mui-selected": {
-            background: 'linear-gradient(to right, #4c46e6, #6d68fe, #b446ff, rgb(151, 0, 255))',
-            color: 'white',
-            borderBottom: 'none',
-        },
-        "& .MuiTab-textColorInherit": {
-            '&:hover': {
-                background: 'linear-gradient(to right, #4c46e6, #6d68fe, #b446ff, rgb(151, 0, 255))',
-                color: 'white',
-            }
-        },
-        borderRadius: '5px',
-        
-    },
+    borderRadius: "5px",
+  },
 }));
